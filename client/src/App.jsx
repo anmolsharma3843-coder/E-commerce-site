51657ff7a95c5df45cf215192aec3fa2c0959ca8
Anmol <anmolsharma3843@gmail.com>
2026-06-19 17:02:23 +0530

    Fix Bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import ScrollToTop from "./components/Scroll";
 import { Suspense } from "react";
